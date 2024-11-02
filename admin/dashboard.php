4e69c4142c453403933f89d74f5e18cd1fd0c21b
Dinuka1220 <earnwithdinu1220@gmail.com>
2024-11-02 23:50:05 +0530

    dashboard changes

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,21 +1,69 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php include '../libraries/styles.php';?>
+        <!-- CSS Files -->
+        <?php include '../libraries/styles.php';?>
 </head>
 <body >
     <!-- <div class="container text-center " style="width:100vw; height:100vh;"> -->
-        <div class="row border border-primary" style="width:100vw; height:100vh;">
-        <div class="row m-auto border border-primary" style="width:60%; height:60%;">
-            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
-            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
-            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
-            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
-            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
-            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
+
+        <div class="row " style="width:100vw; height:100vh;">
+        <div class="row m-auto " style="width:60%; height:60%;">
+            <div class="col-4 align-self-center " style=height:50%; >
+                <!-- <div class="row justify-content-center align-items-center "style="width: 100%;">  -->
+
+                    <div class="col-md-4 d-flex justify-content-center m-auto mt-4"style="width: 100%; height: 80%;">
+                        <div class="card card-secondary" style="width: 100%; height: 100%; cursor: pointer;" role="button" onclick="location.href='registerStudent.php';">
+                            <div class="card-body skew-shadow position-relative">
+                                <h5 class="op-8 text-center position-absolute top-50 start-50 translate-middle fs-2">REGISTER STUDENT</h5>
+                            </div>
+                        </div>
+                    </div>
+                <!-- </div> -->
+
+                </div>
+            
+            <div class="col-4 align-self-center  " style=height:50%; >
+                    <div class="col-md-4 d-flex justify-content-center m-auto mt-4"style="width: 100%; height: 80%;">
+                        <div class="card card-secondary" style="width: 100%; height: 100%; cursor: pointer;" role="button" onclick="location.href='../admin/adlogin.php';">
+                            <div class="card-body skew-shadow position-relative">
+                                <h5 class="op-8 text-center position-absolute top-50 start-50 translate-middle fs-2">VIEW STUDENT LIST</h5>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="col-4 align-self-center  " style=height:50%; >
+                    <div class="col-md-4 d-flex justify-content-center m-auto mt-4"style="width: 100%; height: 80%;">
+                        <div class="card card-secondary" style="width: 100%; height: 100%; cursor: pointer;" role="button" onclick="location.href='../admin/adlogin.php';">
+                            <div class="card-body skew-shadow position-relative">
+                                <h5 class="op-8 text-center position-absolute top-50 start-50 translate-middle fs-2">REGISTER SECURITY PERSON</h5>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="col-4 align-self-center  " style=height:50%; >
+                    <div class="col-md-4 d-flex justify-content-center m-auto mt-4"style="width: 100%; height: 80%;">
+                        <div class="card card-secondary" style="width: 100%; height: 100%; cursor: pointer;" role="button" onclick="location.href='../admin/adlogin.php';">
+                            <div class="card-body skew-shadow position-relative">
+                                <h5 class="op-8 text-center position-absolute top-50 start-50 translate-middle fs-2">VIEW SECURITY LIST</h5>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="col-4 align-self-center  " style=height:50%; >
+                    <div class="col-md-4 d-flex justify-content-center m-auto mt-4"style="width: 100%; height: 80%;">
+                        <div class="card card-secondary" style="width: 100%; height: 100%; cursor: pointer;" role="button" onclick="location.href='../admin/adlogin.php';">
+                            <div class="card-body skew-shadow position-relative">
+                                <h5 class="op-8 text-center position-absolute top-50 start-50 translate-middle fs-2">ROOM DETAILS</h5>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="col-4 align-self-center  " style=height:50%; ></div>
         </div>
         </div>
 
