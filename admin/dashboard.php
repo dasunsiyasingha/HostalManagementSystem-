b2cce96d78cbb1258411fbc4a613d382db7b8a38
Dinuka1220 <earnwithdinu1220@gmail.com>
2024-11-02 14:25:45 +0530

    login ui

---
+++ admin/dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include '../libraries/styles.php';?>
+</head>
+<body >
+    <!-- <div class="container text-center " style="width:100vw; height:100vh;"> -->
+        <div class="row border border-primary" style="width:100vw; height:100vh;">
+        <div class="row m-auto border border-primary" style="width:60%; height:60%;">
+            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
+            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
+            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
+            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
+            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
+            <div class="col-4 align-self-center border border-primary " style=height:50%; ></div>
+        </div>
+        </div>
+
+    <!-- </div> -->
+</body>
+</html>
