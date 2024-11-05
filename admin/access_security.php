e3e6e027ed9cc6b5bb0a285814eee6fa0c850afb
Dinuka1220 <earnwithdinu1220@gmail.com>
2024-11-05 14:13:11 +0530

    access security

--- admin/access_security.php
+++ admin/access_security.php
@@ -32,28 +32,11 @@
   <div class="row  m-auto " style="width:100vw; height:100vh; background-color:#f5f5f5">
     <div class="row  m-auto " style="width:70vw; height:100vh; background-color:#ffffff">
       <div>
-        <h3 class=" card-title text-center mt-3" style="color:#507297">Security Management</h3>
+        <h3 class=" card-title text-center mt-3" style="color:#507297">Mannage Access</h3>
         <hr style="border: 1px solid #507297; width: 100%; mt-3;">
-        <div class="row">
-          <div class="col-6">
-            <button type="button" class="mt-3 ms-5"
-            style="width:80%; height:40px; background-color:#275d8b;border: none; color: white; " data-bs-toggle="modal"
-            data-bs-target="#addRowModal">
-            <i class="bi bi-plus pt-4 pb-5 "></i> Add new security person
-          </button>
-          </div>
-          <div class="col-6">
-            
-          <button type="button" class="mt-3 ms-5"
-            style="width:80%; height:40px; background-color:#275d8b;border: none; color: white; " onclick="location.href='access_security.php';"
-            data-bs-target="#addRowModal">
-            <i ></i> Add new security person
-          </button> 
-          </div>
-       
+      
 
- 
-          <div class="row m-auto">
+      <div class="row m-auto mt-0">
             <!-- <div class="card-body"> -->
 
             <div class="col-md-12">
@@ -104,23 +87,13 @@
                                     placeholder="fill Id" />
                                 </div>
                               </div>
-                              <div class="col-md-6">
-                                <div class="form-group form-group-default">
-                                  <label>Nic</label>
-                                  <input id="addnic" type="text" class="form-control" placeholder="fill Nic Number" />
-                                </div>
-                              </div>
+                            
+
+                            
 
                               <div class="col-md-6">
                                 <div class="form-group form-group-default">
-                                  <label>Age</label>
-                                  <input id="addage" type="text" class="form-control" placeholder="fill age" />
-                                </div>
-                              </div>
-
-                              <div class="col-md-6">
-                                <div class="form-group form-group-default">
-                                  <label>Status</label>
+                                  <label>Give Access</label>
                                   <input id="addstatus" type="text" class="form-control" placeholder="fill age" />
                                 </div>
                               </div>
@@ -152,53 +125,36 @@
                               style="width: 249.613px;">Name</th>
                             <th class="sorting" tabindex="0" aria-controls="add-row" rowspan="1" colspan="1"
                               aria-label="Id: activate to sort column ascending" style="width: 363.612px;">Id</th>
-                            <th class="sorting" tabindex="0" aria-controls="add-row" rowspan="1" colspan="1"
-                              aria-label="Nic: activate to sort column ascending" style="width: 204.275px;">Nic</th>
                             <th style="width: 120.7px;" class="sorting" tabindex="0" aria-controls="add-row" rowspan="1"
-                              colspan="1" aria-label="Action: activate to sort column ascending">Age</th>
-                            <th style="width: 120.7px;" class="sorting" tabindex="0" aria-controls="add-row" rowspan="1"
-                              colspan="1" aria-label="Action: activate to sort column ascending">Status</th>
-                            <th style="width: 120.7px;" class="sorting" tabindex="0" aria-controls="add-row" rowspan="1"
-                              colspan="1" aria-label="Action: activate to sort column ascending">Action</th>
+                              colspan="1" aria-label="Action: activate to sort column ascending">Give Access</th>
+                           
                           </tr>
                         </thead>
                         <tfoot>
                           <tr>
                             <th rowspan="1" colspan="1">Name</th>
                             <th rowspan="1" colspan="1">Id</th>
-                            <th rowspan="1" colspan="1">Nic</th>
-                            <th rowspan="1" colspan="1">Age</th>
-                            <th rowspan="1" colspan="1">Status</th>
-                            <th rowspan="1" colspan="1">Action</th>
+                            
+                            <th rowspan="1" colspan="1">Give Access</th>
+                          
                           </tr>
                         </tfoot>
                         <tbody>
                           <tr role="row" class="odd">
                             <td class="sorting_1">Airi Satou</td>
                             <td>Accountant</td>
-                            <td>Tokyo</td>
-                            <td>54</td>
+                           
+                            
 
                             <td>
                               <div class="form-check form-switch">
-                                <input class="form-check-input status-toggle" type="checkbox" role="switch" id="st_status">
-                                <label class="form-check-label" for="st_status" id="statusLabel"></label>
+                                <input class="form-check-input status-toggle" type="checkbox" role="switch" id="give_access">
+                                <label class="form-check-label" for="give_access" id="statusLabel"></label>
                               </div>
                               
                             </td>
 
-                            <td>
-                              <div class="form-button-action">
-                                <button type="button" data-bs-toggle="tooltip" title=""
-                                  class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
-                                  <i class="fa fa-edit"></i>
-                                </button>
-                                <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-danger"
-                                  data-original-title="Remove">
-                                  <i class="fa fa-times"></i>
-                                </button>
-                              </div>
-                            </td>
+                            
                           </tr>
                           
                         </tbody>
@@ -307,31 +263,6 @@
         });
       </script>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <!-- <div class="row border border-primary" style="width:100vw; height:100vh;"> -->
-      <!-- <div class="row border border-secondary " style="width:100%; height:50%;"> -->
-
-      <!-- <div class="row border border-danger " style="width:100%; height:50%;"><div class="card-action text-center d-flex justify-content-between"> -->
-      <!-- <div>  <button class="btn  mt-5 mb-5 p-3"  style="background-color:#1591ea; width:100%;" >Delete Security</button> -->
-
-      <!-- </div> -->
-
-      <!-- <div class="row border border-danger " style="width:100%; height:50%;"></div> -->
-      <!-- <div> <button  class="btn p-3 ms-5" style="background-color:#1591ea; width:95%;" >ADD Security</button> -->
-
-      <!-- </div> -->
-</body>
-
-</html>
+</div>
+      </body>
+      </html>
